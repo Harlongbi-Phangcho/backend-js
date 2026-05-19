04e40069fa64cb81e4f84a3af5ec2fb789c9e8ab
Harlongbi-Phangcho <harbisonphangcho1999@gmail.com>
2026-05-19 08:53:28 +0530

    feat: add comment functionality with CRUD operations and validation

--- src/app.js
+++ src/app.js
@@ -1,30 +1,32 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
 //accepting json
-app.use(express.json({limit: "16kb"})) //accepting json
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
-
+app.use(express.json({ limit: "16kb" })); //accepting json
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 //routes import
-import userRouter from "./routes/user.routes.js"
-import videoRouter from "./routes/video.routes.js"
-import tweetRouter from "./routes/tweet.routes.js"
-
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 //routes declaration
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/comments", commentRouter);
 
 export { app };
---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,178 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Comment } from "../models/comment.models.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.models.js";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+
+  // Validate videoId
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video ID");
+  }
+
+  const pageNumber = parseInt(page);
+  const limitNumber = parseInt(limit);
+
+  // Validate pagination parameters
+  if (isNaN(pageNumber) || pageNumber < 1) {
+    throw new ApiError(400, "Invalid page number");
+  }
+  if (isNaN(limitNumber) || limitNumber < 1) {
+    throw new ApiError(400, "Invalid limit number");
+  }
+
+  const skipNumber = (pageNumber - 1) * limitNumber;
+
+  const comments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+
+    {
+      $skip: skipNumber,
+    },
+    {
+      $limit: limitNumber,
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comments, "Comments fetched successfully"));
+});
+
+const addComment = asyncHandler(async (req, res) => {
+  const { content } = req.body;
+  const { videoId } = req.params;
+
+  // Validate content
+  if (!content) {
+    throw new ApiError(400, "Content is required");
+  }
+
+  // Validate videoId
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video ID");
+  }
+
+  // find video by id
+  const video = await Video.findOne({
+    _id: videoId,
+  });
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  // Create comment
+  const comment = await Comment.create({
+    content: content.trim(),
+    video: videoId,
+    owner: req.user._id,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment added successfully"));
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  // TODO: update a comment
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  // validate comment id and content
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid comment id");
+  }
+
+  if (!content?.trim()) {
+    throw new ApiError("Content is required");
+  }
+
+  // find comment by id
+  const updatedComment = await Comment.findOneAndUpdate(
+    {
+      _id: commentId,
+      owner: req.user._id,
+    },
+    {
+      $set: {
+        content: content.trim(),
+      },
+    },
+    { new: true }
+  );
+
+  if (!updatedComment) {
+    throw new ApiError(404, "Video not found or unauthorized");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updateComment, "Comment updated successfully"));
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  // TODO: delete a comment
+  const { commentId } = req.params;
+
+  // validate comment id
+  if (!isValidObjectId) {
+    throw new ApiError(400, "Invalid comment id");
+  }
+
+  // find and delete comment
+  const deletedComment = await Comment.findOneAndDelete({
+    _id: commentId,
+    owner: req.user._id,
+  });
+
+  if (!deletedComment) {
+    throw new ApiError(404, "Comment not found or unauthorized");
+  }
+
+  // response
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Comment deleted successfully"));
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
