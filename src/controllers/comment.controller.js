35e38c8bdddf5ec89e702732beb1dd34cbc3b356
Harlongbi-Phangcho <harbisonphangcho1999@gmail.com>
2026-05-20 09:53:41 +0530

    fix: update error message for comment update to reflect correct context

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -142,7 +142,7 @@ const updateComment = asyncHandler(async (req, res) => {
   );
 
   if (!updatedComment) {
-    throw new ApiError(404, "Video not found or unauthorized");
+    throw new ApiError(404, "Comment not found or unauthorized");
   }
 
   return res
