3a1388913d26202a34936eeda3ddfcc3223be2dd
Harlongbi-Phangcho <harbisonphangcho1999@gmail.com>
2026-07-04 15:43:46 +0530

    fix: correct return statement in optionalVerifyJWT middleware

--- src/middlewares/optionalVerifyJWT.middleware.js
+++ src/middlewares/optionalVerifyJWT.middleware.js
@@ -1,24 +1,32 @@
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.models";
-import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.models.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const optionalVerifyJWT = asyncHandler(async (req, res, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
+
     if (!token) {
       req.user = null;
       return next();
     }
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const decodedToken = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
     );
+
     req.user = user || null;
+
+    return next();
   } catch (error) {
-    // Invalid or expired token -> continue as guest
     req.user = null;
-    next();
+    return next();
   }
 });
